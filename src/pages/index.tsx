db0154352569bb081cb568265eea8b2001c085b3
Yuri Weilemann <yuri_weilemann@hotmail.com>
2021-02-23 10:51:28 -0300

    Created Countdown component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Coundown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 
@@ -11,6 +13,8 @@ export default function Home() {
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
+          <Countdown />
         </div>
         <div>
 
