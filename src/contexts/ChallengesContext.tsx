c50f985e1a1da3070bd5d8091bda14880836e1f3
Yuri Weilemann <yuri_weilemann@hotmail.com>
2021-02-24 11:32:42 -0300

    Added challenges.json and randomly disposing challenges after the countdown ends

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,9 +1,17 @@
 import { createContext, useState, ReactNode } from 'react';
+import challenges from '../../challenges.json';
+
+interface Challenge {
+  type: 'body' | 'eye';
+  description: string;
+  amount: number;
+}
 
 interface ChallengesContextData {
   level: number;
   currentExperience: number;
   challengesCompleted: number;
+  activeChallenge: Challenge;
   levelUp: () => void;
   startNewChallenge: () => void;
 }
@@ -18,13 +26,17 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
   const [level, setLevel] = useState(1);
   const [currentExperience, setCurrentExperience] = useState(0);
   const [challengesCompleted, setChallengesCompleted] = useState(0);
+  const [activeChallenge, setActiveChallenge] = useState(null)
 
   function levelUp() {
     setLevel(level + 1);
   }
 
   function startNewChallenge() {
-    console.log('New challenge')
+    const randomChallengeIndex = Math.round(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
+
+    setActiveChallenge(challenge);
   }
 
   return (
@@ -34,7 +46,8 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         currentExperience, 
         challengesCompleted, 
         levelUp,
-        startNewChallenge
+        startNewChallenge,
+        activeChallenge,
       }}
     >
       {children}
