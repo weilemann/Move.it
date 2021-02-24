95a81e41fbb350da1e4537e0f9fdca39eed01d1a
Yuri Weilemann <yuri_weilemann@hotmail.com>
2021-02-24 11:42:22 -0300

    Created context functions and using context variables on respectives components

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -10,10 +10,12 @@ interface Challenge {
 interface ChallengesContextData {
   level: number;
   currentExperience: number;
+  experienceToNextLevel: number;
   challengesCompleted: number;
   activeChallenge: Challenge;
   levelUp: () => void;
   startNewChallenge: () => void;
+  resetChallenge: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -28,26 +30,34 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
   const [challengesCompleted, setChallengesCompleted] = useState(0);
   const [activeChallenge, setActiveChallenge] = useState(null)
 
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
+
   function levelUp() {
     setLevel(level + 1);
   }
 
   function startNewChallenge() {
-    const randomChallengeIndex = Math.round(Math.random() * challenges.length);
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
     const challenge = challenges[randomChallengeIndex];
 
     setActiveChallenge(challenge);
+  }
+
+  function resetChallenge() {
+    setActiveChallenge(null);
   }
 
   return (
     <ChallengesContext.Provider 
       value={{ 
         level, 
-        currentExperience, 
+        currentExperience,
+        experienceToNextLevel,
         challengesCompleted, 
         levelUp,
         startNewChallenge,
         activeChallenge,
+        resetChallenge,
       }}
     >
       {children}
